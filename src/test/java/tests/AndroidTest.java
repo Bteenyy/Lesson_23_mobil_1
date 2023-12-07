db706d19947387142340f102d22bdc46743a8d6a
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-12-07 16:08:45 +0300

    add web driver

--- src/test/java/tests/AndroidTest.java
+++ src/test/java/tests/AndroidTest.java
@@ -1,18 +1,12 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideWait;
-import com.codeborne.selenide.impl.SelenideElementDescriber;
-import com.codeborne.selenide.logevents.SelenideLog;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.*;
 
 public class AndroidTest extends TestBase {
@@ -30,7 +24,7 @@ public class AndroidTest extends TestBase {
         $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
         $(id("org.wikipedia.alpha:id/page_list_item_title")).click();
-
-        //    $(id("torg.wikipedia.alpha:id/container_with_nav_trigger")).shouldNotHave(Condition.text("An error occurred"));
+        $(name("GO BACK")).shouldBe(exist);
+        //$(id("org.wikipedia.alpha:id/page_contents_container")).shouldBe(exist);
     }
 }
