cf5fc7141022ef797282f916ac2c0c8ab1c49816
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-12-07 15:13:22 +0300

    add web driver

--- src/test/java/tests/AndroidTest.java
+++ src/test/java/tests/AndroidTest.java
@@ -1,27 +1,36 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideWait;
+import com.codeborne.selenide.impl.SelenideElementDescriber;
+import com.codeborne.selenide.logevents.SelenideLog;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.appium.java_client.AppiumBy.id;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.AppiumBy.*;
 
 public class AndroidTest extends TestBase {
     @Test
-    @Tag("android")
     void firstTest() {
         $(accessibilityId("Search Wikipedia")).click();
         $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
     }
+
     @Test
     @Tag("android")
     void secondTest() {
         $(accessibilityId("Search Wikipedia")).click();
         $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
+        $(id("org.wikipedia.alpha:id/page_list_item_title")).click();
+
+        //    $(id("torg.wikipedia.alpha:id/container_with_nav_trigger")).shouldNotHave(Condition.text("An error occurred"));
     }
 }
