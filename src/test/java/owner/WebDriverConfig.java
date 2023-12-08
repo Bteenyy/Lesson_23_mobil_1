45dd21902e2366d3d55f3d526ee7906b32856184
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-12-08 14:20:35 +0300

    add web driver

--- src/test/java/owner/WebDriverConfig.java
+++ src/test/java/owner/WebDriverConfig.java
@@ -18,14 +18,11 @@ public interface WebDriverConfig extends Config {
     String getUrl();
 
     @Key("browserstack.device")
-    @DefaultValue("Google Pixel 3")
     String getDevice();
 
     @Key("browserstack.os_version")
-    @DefaultValue("9.0")
     String getOS();
 
     @Key("browserstack.app")
- //   @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
     String getApp();
 }
